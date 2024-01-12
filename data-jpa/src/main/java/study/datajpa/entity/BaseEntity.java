71c6dda34e2b132858568491076198040237dcdf
Jaeuk Oh <wooko5@naver.com>
2024-01-12 11:25:59 +0900

    gradle 파일변경

--- data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
+++ data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
@@ -2,20 +2,17 @@ package study.datajpa.entity;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class) //해당 어노테이션이 있어야 데이터의 변경(C/U/D) 시 이벤트(@PreUpdated...)를 발생
 @Getter
 @MappedSuperclass
-public class BaseEntity extends BaseTimeEntity{
+public class BaseEntity extends BaseTimeEntity {
 
     @CreatedBy
     @Column(updatable = false)
